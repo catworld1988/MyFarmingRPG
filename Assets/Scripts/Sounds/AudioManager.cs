6ec16063893cd3b1713ddb08ded9fb729035966a
MS-LGMESZNDZSNE\Administrator <catworld.s@163.com>
2022-09-28 23:49:10 +0800

    混音台输出 场景之间的环境音和背景音乐切换

--- Assets/Scripts/Sounds/AudioManager.cs
+++ Assets/Scripts/Sounds/AudioManager.cs
@@ -1,13 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : SingletonMonobehaviour<AudioManager>
 {
     [SerializeField] private GameObject soundPrefab = null;
-    [Header("Other")] [SerializeField] private SO_SoundList so_soundList = null;
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource ambientSoundAudioSource = null;
+
+    [SerializeField] private AudioSource gameMusicAudioSource = null;
+
+    [Header("Audio Mixers")]
+    [SerializeField] private AudioMixer gameAudioMixer = null;
+
+    [Header("Audio Snapshots")]
+    [SerializeField] private AudioMixerSnapshot gameMusicSnapshot = null;
+
+    [SerializeField] private AudioMixerSnapshot gameAmbientSnapshot = null;
+
+    [Header("Other")]
+    [SerializeField] private SO_SoundList so_soundList = null;
+
+    [SerializeField] private SO_SceneSoundsList so_SceneSoundsList = null;
+    [SerializeField] private float defaultSceneMusicPlayTimeSeconds = 120f;
+    [SerializeField] private float sceneMusicStartMinSecs = 20f;
+    [SerializeField] private float sceneMusicStartMaxSecs = 40f;
+    [SerializeField] private float musicTransitionSecs = 8f;
 
     private Dictionary<SoundName, SoundItem> soundDictionary;
+    private Dictionary<SceneName, SceneSoundsItem> sceneSoundsDictionary;
+
+    private Coroutine playSceneSoundsCoroutine;
 
     protected override void Awake()
     {
@@ -22,7 +49,111 @@ public class AudioManager : SingletonMonobehaviour<AudioManager>
             soundDictionary.Add(soundItem.soundName, soundItem);
         }
 
+        //初始化 场景音乐字典
+        sceneSoundsDictionary = new Dictionary<SceneName, SceneSoundsItem>();
 
+        //so_SceneSoundsList填充到字典
+        foreach (SceneSoundsItem sceneSoundsItem in so_SceneSoundsList.SceneSoundsDetails)
+        {
+            sceneSoundsDictionary.Add(sceneSoundsItem.SceneName,sceneSoundsItem);
+        }
+    }
+
+    private void OnEnable()
+    {
+        EventHandler.AfterSceneLoadEvent += playSceneSounds;
+    }
+
+    private void OnDisable()
+    {
+        EventHandler.AfterSceneLoadEvent -= playSceneSounds;
+    }
+
+    private void playSceneSounds()
+    {
+        //等待填充的变量
+        SoundItem musicSoundItem = null;
+        SoundItem ambientSoundItem = null;
+
+        float musicPlayTime = defaultSceneMusicPlayTimeSeconds;
+
+        //检测正确的场景
+        if (Enum.TryParse<SceneName>(SceneManager.GetActiveScene().name,true,out SceneName currentSceneName))
+        {
+            //获取场景音乐和环境音
+            if (sceneSoundsDictionary.TryGetValue(currentSceneName,out SceneSoundsItem sceneSoundsItem))
+            {
+                soundDictionary.TryGetValue(sceneSoundsItem.musicForScene, out musicSoundItem);
+                soundDictionary.TryGetValue(sceneSoundsItem.ambientSoundForScene, out ambientSoundItem);
+            }
+            else
+            {
+                return;
+            }
+
+            //有场景声音的话 停止场景声音
+            if (playSceneSoundsCoroutine !=null)
+            {
+                StopCoroutine(playSceneSoundsCoroutine);
+            }
+
+            playSceneSoundsCoroutine = StartCoroutine(PlaySceneSoundsCoroutine(musicPlayTime, musicSoundItem, ambientSoundItem));
+        }
+    }
+
+    private IEnumerator PlaySceneSoundsCoroutine(float musicPlaySeconds, SoundItem musicSoundItem, SoundItem ambientSoundItem)
+    {
+        if (musicSoundItem !=null && ambientSoundItem !=null)
+        {
+            //开始环境声
+            PlayAmbientSoundClip(ambientSoundItem, 0f);
+
+            //随机等待几秒
+            yield return new WaitForSeconds(UnityEngine.Random.Range(sceneMusicStartMinSecs, sceneMusicStartMaxSecs));
+
+            //然后播放音乐
+            PlayMusicSoundClip(musicSoundItem, musicTransitionSecs);
+
+            //播放环境音前等待
+            yield return new WaitForSeconds(musicPlaySeconds);
+
+            //播放环境音乐剪辑
+            PlayAmbientSoundClip(ambientSoundItem, musicTransitionSecs);
+        }
+    }
+
+    private void PlayMusicSoundClip(SoundItem musicSoundItem, float transitionTimeSeconds)
+    {
+        //设置混音台 暴露出来的音乐音量参数 转换小数为分贝
+        gameAudioMixer.SetFloat("MusicVolume", ConverSoundVolumeDecimalFractionToDecibels(musicSoundItem.soundVolume));
+
+        //设置剪辑 并播放
+        gameMusicAudioSource.clip = musicSoundItem.soundClip;
+        gameMusicAudioSource.Play();
+
+        // 过渡 按照时间差值 过渡到 混音台音乐快照
+        gameMusicSnapshot.TransitionTo(transitionTimeSeconds);
+    }
+
+    private void PlayAmbientSoundClip(SoundItem ambientSoundItem, float transitionTimeSeconds)
+    {
+        //设置混音台 暴露出来的参数 转换小数为分贝
+        gameAudioMixer.SetFloat("AmbientVolume", ConverSoundVolumeDecimalFractionToDecibels(ambientSoundItem.soundVolume));
+
+        //设置剪辑 并播放
+        ambientSoundAudioSource.clip = ambientSoundItem.soundClip;
+        ambientSoundAudioSource.Play();
+
+        //按照时间差值 过渡到 环境声快照
+        gameAmbientSnapshot.TransitionTo(transitionTimeSeconds);
+    }
+
+    /// <summary>
+    /// 转换小数到分贝 -80db to +20db之间
+    /// </summary>
+    private float ConverSoundVolumeDecimalFractionToDecibels(float volumDecimalFraction)
+    {
+        return (volumDecimalFraction * 100f - 80f);
     }
 
     public void PlaySound(SoundName soundName)
---
+++ Assets/Scripts/Sounds/SceneSoundsItem.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class SceneSoundsItem
+{
+    public SceneName SceneName;
+    public SoundName ambientSoundForScene;
+    public SoundName musicForScene;
+}
